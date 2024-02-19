74fb76e9d72a7a7c03d21f1ff8abfae41ca70337
Sakuntha <sakunthadayadara2001@gmail.com>
2024-02-20 02:48:44 +0530

    Migrate to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_19_163613) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "taxes", force: :cascade do |t|
     t.decimal "grossincome"
     t.decimal "taxdeduction"
